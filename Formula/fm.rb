a804ecb148980d15b046ad1fced835c19767856b
MaAmos <114552781+MaAmos@users.noreply.github.com>
2025-10-20 15:32:49 +0800

    Update fm.rb

--- Formula/fm.rb
+++ Formula/fm.rb
@@ -2,7 +2,7 @@ class Fm < Formula
   desc "文件查询 CLI"
   homepage "https://github.com/MaAmos/fm"
   url "https://github.com/MaAmos/fm/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "53dd386d9206648913b6b728bbde5fbf447f2179d24d454f3452e85b0c43f2bc"
+  sha256 "804b67082ea03ebe637b2a7f202bb8e8a73a6206c595273aafb8b3b75933ef84"
   license "MIT" # 按你的项目实际 license
 
   depends_on "rust" => :build
@@ -15,4 +15,5 @@ class Fm < Formula
     # 最简单的健壮性测试：能跑起来并返回0
     system "#{bin}/fm", "--help"
   end
+
 end
